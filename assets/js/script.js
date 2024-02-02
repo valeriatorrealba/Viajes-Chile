46c4d94151bf93dccced392bfc4132a1eca12175
Valeria Torrealba <valeria.torrealba@yahoo.cl>
2024-02-02 00:45:07 -0300

    se agregaron comentarios se cambio el carrusel que se mueva, se agregaron tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+// Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+
+// Enviar correo
 $('#enviarcorreo').on("click",function(){
     alert("El correo fue enviado correctamente...");
 });
